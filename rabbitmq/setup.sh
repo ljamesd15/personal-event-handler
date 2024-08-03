7948ad77762a8565cccc614240291166df97aab3
James Davidson <littleton.davidson@gmail.com>
2024-08-02 20:00:04 -0400

    Update rabbitmq setup

--- rabbitmq/setup.sh
+++ rabbitmq/setup.sh
@@ -20,6 +20,7 @@ wget http://127.0.0.1:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
 sudo mv rabbitmqadmin /etc/rabbitmq
 export PATH="$PATH:/etc/rabbitmq/"
+sudo mv ~/Downloads/rabbitmq_delayed_message_exchange-3.13.0.ez /usr/lib/rabbitmq/lib/rabbitmq_server-3.13.6/plugins/
 sudo rabbitmq-plugins enable rabbitmq_delayed_message_exchange
 
 # Create users and virtual hosts
@@ -40,5 +41,4 @@ rabbitmqadmin declare exchange --vhost="$WEATHER_RABBITMQ_VHOST" name="$WEATHER_
 
 # Test MQTT publish and bindings
 mosquitto_pub -h localhost -t weather -m '{"time":"2024-02-21T23:37:46.410Z","temperature":0,"pressure":0,"humidity":0,"windSpeed":0,"windDirection":"N","luminosity":0,"uvIndex":0,"sensorMetadata":{"longitude":0,"latitude":0,"sensorId":"string","tags":["string"],"location":"string"}}' -q 1 -p 1883 -u "$WEATHER_RABBITMQ_PUBLISHER" -P "$WEATHER_RABBITMQ_PUBLISHER_PASSWORD"
-mosquitto_pub -h localhost -t weather -m '{"temperature":0,"pressure":0,"humidity":0,"windSpeed":0,"windDirection":"N","luminosity":0,"uvIndex":0,"sensorMetadata":{"longitude":0,"latitude":0,"sensorId":"string","tags":["string"],"location":"string"}}' -q 1 -p 1883 -u "$WEATHER_RABBITMQ_PUBLISHER" -P "$WEATHER_RABBITMQ_PUBLISHER_PASSWORD"
-sudo mv ~/Downloads/rabbitmq_delayed_message_exchange-3.12.0.ez /usr/lib/rabbitmq/lib/rabbitmq_server-3.12.13/plugins/
+mosquitto_pub -h localhost -t weather -m '{"time":"2024-02-21T23:37:46.410Z","temperature":0,"pressure":0,"humidity":0,"windSpeed":0,"windDirection":"N","luminosity":0,"uvIndex":0,"sensorMetadata":{"longitude":0,"latitude":0,"sensorId":"string","tags":["string"],"location":"string"}}' -q 1 -p 1883 -u "$WEATHER_RABBITMQ_PUBLISHER" -P "$WEATHER_RABBITMQ_PUBLISHER_PASSWORD"
